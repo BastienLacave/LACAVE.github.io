a6335f8b6303974533393cd7d68d5f455a6af21c
BastienLacave <100997257+BastienLacave@users.noreply.github.com>
2024-04-07 11:03:20 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"97550907b70f4f3b328b6c1600df21fac1a1889a","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"97550907b70f4f3b328b6c1600df21fac1a1889a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3021357609"
+    serviceWorkerVersion: "2291689676"
   }
 });
